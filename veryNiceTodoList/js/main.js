fb35a1585dd50e68af62359fb7b255de42d32aba
98savage <nikita-anoshin@yandex.ru>
2020-05-07 22:17:57 +0300

    add localStorage

---
+++ veryNiceTodoList/js/main.js
@@ -0,0 +1,44 @@
+let list = document.querySelector('ul');
+
+let todoStorage; 
+function todoLocal() {
+	todoStorage = list.innerHTML;
+	localStorage.setItem('todoStorage', todoStorage);
+}
+
+list.addEventListener('click', function(ev){
+	if(ev.target.tagName === 'LI'){
+		ev.target.classList.toggle('checked');
+		todoLocal();
+	}
+	else if(ev.target.tagName === 'SPAN'){
+			let div = ev.target.parentNode;
+			div.remove();
+		todoLocal();
+		}
+	
+}, false);
+
+function newElement() {
+	let li = document.createElement('li');
+	let inputValue = document.getElementById('toDoEl').value;
+	let t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if(inputValue == ''){
+		alert('Вы ничего не написали, напишите то, что хотите сделать!');
+	}
+	else{
+		document.getElementById('list').appendChild(li);
+	}
+	document.getElementById('toDoEl').value = '';
+	let span = document.createElement('SPAN');
+	let txt = document.createElement('SPAN');
+	span.className = 'close';
+	span.appendChild(txt);
+	li.appendChild(span);
+	todoLocal();
+	}
+
+	if(localStorage.getItem('todoStorage')) {
+		list.innerHTML = localStorage.getItem('todoStorage');
+	}
